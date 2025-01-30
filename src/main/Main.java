a01735a56bf1d01099c72818d2c0b7fc74e623cf
JAJH <72751999+juann17-afk@users.noreply.github.com>
2025-01-30 13:21:19 +0100

    menumain

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,5 @@
 package main;
 
-import pizzeria.Cliente;
 import java.util.Scanner;
 
 public class Main {
@@ -11,17 +10,18 @@ public class Main {
 		
 		System.out.println("Bienvenido a la Pizzeria del EquipoA");
 		do {
+		pizzeria.MenuMain.menu();;
 		m1 = sca.nextInt();
 		
 		if(m1 == 1) {
 			System.out.println("PIZZAS");
-			System.out.println("Escoga una opciï¿½n:\n"
+			System.out.println("Escoga una opción:\n"
 					+ "1. Nueva Piezza\n"
 					+ "2. Ver Pizzas\n"
 					+ "3. Buscar Pizzas con Ingredientes\n"
 					+ "4. Buscar Pizzas sin Ingredientes\n"
-					+ "5. Pizza mï¿½s Cara\n"
-					+ "6. Pizza mï¿½s Barata\n"
+					+ "5. Pizza más Cara\n"
+					+ "6. Pizza más Barata\n"
 					+ "7. Volver");
 			int m2 = sca.nextInt();
 			if(m2 == 7) {
@@ -29,4 +29,5 @@ public class Main {
 		}
 		}while(m1 != 4);
 	}
+
 }
