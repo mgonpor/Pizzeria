d2462375d69d82442755cdaaa4ebd7e5aa57b2d7
franvergara10 <vergarafran12345@gmail.com>
2025-01-30 12:59:28 +0100

    n

--- src/pizzeria/Pizza.java
+++ src/pizzeria/Pizza.java
@@ -4,12 +4,12 @@ public class Pizza {
 	private String nombre;
 	private String ingredientes;
 	private double precio;
-	
+
 	public Pizza(String nombre, double precio, String ingredientes) {
 		setIngredientes(ingredientes);
 		setNombre(nombre);
 		setPrecio(precio);
-		
+
 	}
 
 	public String getNombre() {
@@ -17,24 +17,22 @@ public class Pizza {
 	}
 
 	public void setNombre(String nombre) {
-		if(nombre == null || nombre.isBlank()) {
+		if (nombre == null || nombre.isBlank()) {
 			throw new IllegalArgumentException("El nombre de las pizzas está en blanco o nulo");
 		}
-			this.nombre = nombre;	
-		}
-	
+		this.nombre = nombre;
+	}
 
 	public String getIngredientes() {
 		return ingredientes;
 	}
 
 	public void setIngredientes(String ingredientes) {
-		if(ingredientes == null || ingredientes.isBlank()) {
+		if (ingredientes == null || ingredientes.isBlank()) {
 			throw new IllegalArgumentException("El nombre de los ingredientes está en blanco o nulo");
 		}
-			this.ingredientes = ingredientes;	
-		
-		
+		this.ingredientes = ingredientes.toUpperCase();
+
 	}
 
 	public double getPrecio() {
@@ -42,12 +40,11 @@ public class Pizza {
 	}
 
 	public void setPrecio(double precio) {
-		if(precio<=0.0) {
+		if (precio <= 0.0) {
 			throw new IllegalArgumentException("El precio de la pizz es menor o igual cero");
 		}
-			this.precio = precio;
-		
-		
+		this.precio = precio;
+
 	}
 
 }
--- src/pizzeria/Pizzeria.java
+++ src/pizzeria/Pizzeria.java
@@ -1,5 +1,28 @@
 package pizzeria;
 
-public class Pizzeria {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Pizzeria {
+	private String nombre;
+	private List<Pizza> listaPizzas;
+	private List<Pedido> listaPedidos;
+	private List<Cliente> listaClientes;
+	
+	public Pizzeria(String nombre) {
+		setNombre(nombre);
+		this.listaClientes=new ArrayList<Cliente>();
+		this.listaPedidos=new ArrayList<Pedido>();
+		this.listaPizzas=new ArrayList<Pizza>();
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+ 
 }
